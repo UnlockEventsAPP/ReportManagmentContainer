5638703314da52fac8ac3f5435add63f8efbe161
MrDesilvestri <carlosdesilvestrir@javeriana.edu.co>
2024-11-19 19:04:36 -0500

    Fix: fixing cross-origin problem.

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 from database import create_reports_tables
 from routers import reports
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://front-unlock-patrones.vercel.app",  # Dominio de tu frontend
+    "http://localhost:4200",  # Si estás probando en Angular localmente
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Permitir estos orígenes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 @app.on_event("startup")
 def on_startup():
